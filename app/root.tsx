a52ecbc342ae794cfcac485da5a1dc994989816e
Francisco Mesa <francisco.mc.ids@gmail.com>
2026-04-10 15:26:05 -0400

    style: Update theme color meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -53,7 +53,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#0a0a0a" />
+        <meta name="theme-color" content="#131313" />
         <meta name="application-name" content="Escruta" />
         <meta name="format-detection" content="telephone=no" />
         <Meta />
